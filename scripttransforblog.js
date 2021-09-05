58b454bc5745cdaa36be8306c291467faede04e6
gurubaru <71360199+gurubaru@users.noreply.github.com>
2021-09-05 15:33:20 +0800

    Update scripttransforblog.js

--- scripttransforblog.js
+++ scripttransforblog.js
@@ -1,9 +1,9 @@
 var text = '';
 if (!$.browser.mozilla) text = 'Perhatian: Peramban Anda bukan Firefox. Mungkin Anda tidak dapat melihat aksara Bali di halaman ini dengan benar. Silakan baca <a href="http://jv.wikipedia.org/wiki/Pitulung:Aksara_Jawa">halaman bantuan ini</a>'
-$('#bd').before('<div>' + text + '</div>');
+$('#bdtutlenc').before('<div>' + text + '</div>');
 
 function init_document() {
-    var element = document.getElementById("ta");
+    var element = document.getElementById("tatutlenc");
     var jv = DoTransliterate(element.value);
     var pre_view_element = document.getElementById("prev_labeltutlencblog");
     pre_view_element.innerHTML = jv;
@@ -14,18 +14,18 @@ function DoPreview() {
     $("#prev_labeltutlencblog").text(trans_text);
 }
 function DoAppendTransliteration() {
-    var currentText = $("#ta").val();
+    var currentText = $("#tatutlenc").val();
 
     var text = $("#inp_txttutlencblog").val();
     if (text.toLowerCase() == 'clear') {
         /*typing clear in the inputbox is very tempting.. lets write a hack.*/
         $("#inp_txttutlencblog").val('');
-        $("#ta").val('');
+        $("#tatutlenc").val('');
         return;
     }
     var trans_text = DoTransliterate(text);
     var finalText = currentText + ' ' + trans_text;
-    $("#ta").val(finalText);
+    $("#tatutlenc").val(finalText);
     $("#inp_txttutlencblog").val('');
 }
 $(document).ready(function () {
